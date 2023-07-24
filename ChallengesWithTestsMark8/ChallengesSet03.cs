725c707916ada301689d6244cb853d9302c23a21
eopong87 <eopong87@gmail.com>
2023-07-24 00:43:40 -0400

    week4

--- ChallengesWithTestsMark8/ChallengesSet03.cs
+++ ChallengesWithTestsMark8/ChallengesSet03.cs
@@ -1,53 +1,108 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ChallengesWithTestsMark8
 {
     public class ChallengesSet03
     {
         public bool ArrayContainsAFalse(bool[] vals)
-        {
-            throw new NotImplementedException();
+        {//vals.Contains(false);
+
+            for (int i = 0; i < vals.Length; i++)
+            {
+                if (vals[i] == false)
+                {
+                    return true;
+                }
+            }
+            return false;
         }
 
         public bool IsSumOfOddsOdd(IEnumerable<int> numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null || numbers.Count() == 0)
+            {
+                return false;
+            }
+
+            var sum = numbers.Sum();
+
+            return (sum % 2 != 0);
         }
 
         public bool PasswordContainsUpperLowerAndNumber(string password)
-        {
-            throw new NotImplementedException();
+
+        {//password.Any(char.IsLower) && password.Any(char.IsUpper) && password.Any(char.IsNumber);
+            var isLower = false;
+            var isUpper = false;
+            var isNumber = false;
+            for (int i = 0; i < password.Length; i++)
+            {
+                if (char.IsLower(password[i]))
+                {
+                    isLower = true;
+                }
+                if (char.IsUpper(password[i]))
+                {
+                    isUpper = true;
+                }
+                if (char.IsNumber(password[i]))
+                {
+                    isNumber = true;
+                }
+            }
+            if (isLower && isUpper && isNumber)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
 
         public char GetFirstLetterOfString(string val)
         {
-            throw new NotImplementedException();
-        }
-
-        public char GetLastLetterOfString(string val)
-        {
-            throw new NotImplementedException();
+            return val.First();
         }
 
         public decimal Divide(decimal dividend, decimal divisor)
         {
-            throw new NotImplementedException();
+            return dividend / divisor;
         }
 
         public int LastMinusFirst(int[] nums)
         {
-            throw new NotImplementedException();
+            return nums[nums.Length - 1] - nums[0];
         }
 
         public int[] GetOddsBelow100()
-        {
-            throw new NotImplementedException();
+        {// single line Enumerable.Range(1, 99).Where(x => x % 2 != 0).ToArray();
+            var list = new List<int>();
+
+            for (int i = 0; i < 100; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    list.Add(i);
+                }
+            }
+            return list.ToArray();
         }
 
         public void ChangeAllElementsToUppercase(string[] words)
         {
-            throw new NotImplementedException();
+            for (int i = 0; i < words.Length; i++)
+            {
+                words[i] = words[i].ToUpper();
+            }
+        }
+
+        private static char GetLastLetterOfString(string val)
+        {
+            return val[val.Length - 1];
         }
     }
 }
+
--- ChallengesWithTestsMark8/ChallengesSet04.cs
+++ ChallengesWithTestsMark8/ChallengesSet04.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ChallengesWithTestsMark8
 {
@@ -6,47 +8,119 @@ namespace ChallengesWithTestsMark8
     {
         public int AddEvenSubtractOdd(int[] numbers)
         {
-            throw new NotImplementedException();
+            {
+
+
+                int Total = 0;
+
+                foreach (int num in numbers)
+                {
+                    if (num % 2 == 0)
+                        Total += num;
+                    else
+                        Total -= num;
+                }
+
+                return Total;
+            }
         }
 
         public int GetLengthOfShortestString(string str1, string str2, string str3, string str4)
         {
-            throw new NotImplementedException();
+            List<string> stringLlist = new List<string>() { str1, str2, str3, str4 };
+            return stringLlist.Min(x => x.Length);
         }
 
         public int GetSmallestNumber(int number1, int number2, int number3, int number4)
         {
-            throw new NotImplementedException();
+            int[] array = new int[] { number1, number2, number3, number4 };
+            return array.Min();
         }
 
         public void ChangeBusinessNameTo_TrueCoders(Business biz)
         {
-            throw new NotImplementedException();
+            biz.Name = "TrueCoders";
         }
 
         public bool CouldFormTriangle(int sideLength1, int sideLength2, int sideLength3)
         {
-            throw new NotImplementedException();
+            if (sideLength1 + sideLength2 > sideLength3 &&
+           sideLength3 + sideLength1 > sideLength3 &&
+           sideLength2 + sideLength3 > sideLength1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+
         }
 
         public bool IsStringANumber(string input)
         {
-            throw new NotImplementedException();
+
+            double n;
+            bool isNumber = double.TryParse(input, out n);
+
+            return isNumber;
         }
 
         public bool MajorityOfElementsInArrayAreNull(object[] objs)
         {
-            throw new NotImplementedException();
+            return objs.Where(x => x == null).Count() > (objs.Count() / 2);
         }
-
         public double AverageEvens(int[] numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null || numbers.Length == 0)
+            { return 0; }var n = numbers.Where(x => x % 2 == 0);
+            {
+                return 0;
+            }
+            return n.Average();
         }
-
         public int Factorial(int number)
         {
-            throw new NotImplementedException();
+            var fact = 1;
+            if( number < 0)
+            {
+                throw new ArgumentException();
+            }
+            if (number == 1 || number == 0)
+            {
+                return 1;
+            }
+            if (number >=2)
+            {
+                var factorial = number * Factorial(number - 1);
+                return factorial;
+            }
+            return 0; 
+
+
+
+
         }
-    }
-}
+} }
+
+
+   
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
